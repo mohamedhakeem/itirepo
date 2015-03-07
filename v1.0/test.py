4c48f229c5095e1d07d0dbf40f1fdc46efe7e4c7
mohamed <m3.kabeel@gmail.com>
2015-03-07 19:05:49 +0200

    second commit

--- v1.0/test.py
+++ v1.0/test.py
@@ -9,14 +9,14 @@
 #   else:                  # else part of the loop
 #      print num, 'is a prime number'
 
-# i = 2
-# while(i < 100):
-#    j = 2
-#    while(j <= (i/j)):
-#       if not(i%j): break
-#       j = j + 1
-#    if (j > i/j) : print i, " is prime"
-#    i = i + 1
+i = 2
+while(i < 100):
+   j = 2
+   while(j <= (i/j)):
+      if not(i%j): break
+      j = j + 1
+   if (j > i/j) : print i, " is prime"
+   i = i + 1
 
 # print "Good bye!"
 # for letter in 'Python':     # First Example
